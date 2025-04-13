0877f3aca64bbdad7cc960923163ff9f384d9ded
kunshika <kunshika1803@gmail.com>
2025-04-14 01:07:04 +0530

    useRef

--- projects/6-todo-app-version-three/src/Components/AppName.jsx
+++ projects/6-todo-app-version-three/src/Components/AppName.jsx
@@ -1,6 +1,6 @@
 import styles from "./AppName.module.css";
 function AppName() {
-  return <h1 className={styles.todoHeading}>TODO app</h1>;
+  return <h1 className={styles.todoHeading}>TODO App</h1>;
 }
 
 export default AppName;
